472954480d98665f44e1b79bc130bc5cf68dd7ad
joemust0 <joaofss.1991@gmail.com>
2024-06-08 10:59:52 -0300

    Criação de projeto inicial java spring.

---
+++ src/test/java/br/com/etec/projetoquickstartguide/ProjetoquickstartguideApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etec.projetoquickstartguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoquickstartguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
